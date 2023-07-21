985a9724b277a01f5c43f36bad3f3de79b3a01d4
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-07-21 09:50:24 -0500

    added updated SecurityConfig Lambda DSL SecurityFilterChain

--- src/main/java/dev/kbekind/springsecuritylamdadsl/security/SecurityConfig.java
+++ src/main/java/dev/kbekind/springsecuritylamdadsl/security/SecurityConfig.java
@@ -1,13 +1,28 @@
 package dev.kbekind.springsecuritylamdadsl.security;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests( auth -> {
+                    auth.requestMatchers("/").permitAll();
+                    auth.requestMatchers("/oollection/**").hasRole("COLLECTOR");
+                    auth.anyRequest().authenticated();
 
+                })
+
+                .build();
+
+    }
 
 }
