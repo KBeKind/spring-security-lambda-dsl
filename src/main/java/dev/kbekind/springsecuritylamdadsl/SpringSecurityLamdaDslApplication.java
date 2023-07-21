372e1689ce8600c92f77347da911574a75664c11
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-07-21 09:08:26 -0500

    initial commit

---
+++ src/main/java/dev/kbekind/springsecuritylamdadsl/SpringSecurityLamdaDslApplication.java
@@ -0,0 +1,13 @@
+package dev.kbekind.springsecuritylamdadsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLamdaDslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLamdaDslApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kbekind/springsecuritylamdadsl/SpringSecurityLamdaDslApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kbekind.springsecuritylamdadsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLamdaDslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
